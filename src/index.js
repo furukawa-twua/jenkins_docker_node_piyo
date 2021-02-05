501d4bf1f371cdc91a5658f6d6d5f05dd134c296
furukawa_twua <kbc17a19@stu.kawahara.ac.jp>
2021-02-05 11:14:47 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const request = require("request");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+request("https://www.nikkei.com/markets/worldidx/chart/nk225/", (err, res, body) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    try {
+        const dom = new JSDOM(body);
+        const nikkei = dom.window.document.getElementsByClassName("economic_value_now")[0].innerText;
+        console.log(nikkei);
+    } catch (e) {
+        console.error(e);
+    }
+});
